664005ff73138e34ddfadcf0062f5f642a50c031
zhengwenhui <zhengwenhui@outlook.com>
2013-10-10 19:22:15 +0800

    no replay animation

--- src/com/zhengwenhui/toiletpaper/MainActivity.java
+++ src/com/zhengwenhui/toiletpaper/MainActivity.java
@@ -3,6 +3,7 @@ package com.zhengwenhui.toiletpaper;
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -16,6 +17,8 @@ public class MainActivity extends Activity {
 			R.anim.tp3,
 	};
 	private int index =0;
+	private Handler finishHandler;
+	private boolean mRunAnim = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +36,35 @@ public class MainActivity extends Activity {
 	}*/
 
 	public void onClickButton(View view){
+		if(mRunAnim){
+			return;
+		}
 		mImageButton.setImageResource(mAnimResId[index++]);
 		mAnimationDrawable = (AnimationDrawable) mImageButton.getDrawable();  
+
 		if(mAnimationDrawable.isRunning()){
 			mAnimationDrawable.stop();
 		}
 		mAnimationDrawable.start();
-		
+		mRunAnim = true;
+		finishHandler = new Handler();
+		finishHandler.postDelayed(
+				new Runnable() {
+					public void run() {
+						mRunAnim = false;
+					}
+				}, getTotalDuration(mAnimationDrawable));
+
 		if(index>=mAnimResId.length){
 			index = 0;
 		}
+	}
+
+	public int getTotalDuration(AnimationDrawable ad) {
+		int durationTime = 0;
+		for (int i = 0; i < ad.getNumberOfFrames(); i++) {
+			durationTime += ad.getDuration(i);
+		}
+		return durationTime;
 	}
 }
